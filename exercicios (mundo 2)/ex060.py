3cd39d30a23eefb8a495ef876d678f6f9d62af47
lucasalmeida87222-del <lucasalmeida87222@gmail.com>
2026-08-10 08:59:41 -0300

    Realizei alterações

--- exercicios (mundo 2)/ex060.py
+++ exercicios (mundo 2)/ex060.py
@@ -1,12 +1,18 @@
 #Faça um programa que leia um numero qualquer e mostre o seu fatorial.
 #Ex: 5! = 5x4x3x2x1 = 120
-numero = int(input('Digite um valor inteiro: '))
-c = numero
-fatorial = 1
-print(f'Calculando {numero}! = ',end=' ')
+from math import factorial
+n = int(input('Digite um número para calcular seu Fatorial: '))
+f = factorial(n)
+print(f'O fatorial de {n} é {f}.')
+
+#outro método
+n = int(input('Digite um número para calcular seu Fatorial: '))
+c = n
+f = 1
+print(f'Calculando {n}! = ', end=' ')
 while c > 0:
-    print(f'{c}',end=' ')
-    print('x' if c > 1 else '=',end=' ')
-    fatorial *= c
+    print(f'{c}', end=' ')
+    print(' x ' if c > 1 else ' = ', end=' ')
+    f *= c
     c -= 1
-    print(f'{fatorial}')
+print(f'{f}')
--- exercicios (mundo 2)/ex061.py
+++ exercicios (mundo 2)/ex061.py
@@ -1,2 +1,6 @@
 #Refaça o desafio 051, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da progressão 
 #usando a estrutura while.
+termo = int(input('Digite o primeiro termo de uma PA: '))
+razao = int(input('Digite a razao de uma PA: '))
+decimo = termo + (10 - 1) * razao
+c = 10
