e4d66849d20ea1d582b4eb18fcc77324994e2bf3
lucasalmeida87222-del <lucasalmeida87222@gmail.com>
2026-08-12 08:52:29 -0300

    Atualização

--- exercicios (mundo 2)/ex063.py
+++ exercicios (mundo 2)/ex063.py
@@ -1,8 +1,12 @@
 #Escreva um programa que leia um número n inteiro qualquer e mostre na tela os n primeiros elementos
 #de uma sequência de Fibonacci. Ex: 0 1 1 2 3 5 8.
+print('-'*30)
+print('Sequência de Fibonacci')
+print('-'*30)
 n = int(input('Quantos termos você quer mostrar? '))
 t1 = 0
 t2 = 1
+print('~'*30)
 print(f'{t1} -> {t2}', end='')
 cont = 3  
 while cont <= n:
--- exercicios (mundo 2)/ex065.py
+++ exercicios (mundo 2)/ex065.py
@@ -1,3 +1,40 @@
 #Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a
 #média entre todos os valores e o menor valor lido. O programa deve perguntar ao usuário se ele 
 #quer ou não continuar a digitar valores.
+n = 's'.upper().strip()
+soma = 1
+cont = 0
+num = int(input('Digite um valor: '))
+menor = num
+while n != 'N':
+    num = int(input('Digite um valor: '))
+    soma += num
+    cont += 1
+    n = str(input('Quer continuar? [S/N] ')).upper()
+    if menor < num:
+        menor = menor
+    else:
+        menor = num
+    if n == 'N':
+        break
+media = soma / cont
+print(f'O valor total foi de {soma:.2f} e a média desse valor é {media:.2f} e o menor valor digitado foi {menor}')
+
+#Resolução do professor
+resp = 'S'
+soma = quant = media = maior = menor = 0
+while resp in 'Ss':
+    num = int(input('Digite um numero: '))
+    soma += num
+    quant += 1
+    if quant == 1:
+        maior = menor = num
+    else:
+        if num > maior:
+            maior = num
+        if num < menor:
+            menor = num
+    resp = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+media = soma / quant
+print(f'Você digitou {quant} numeros e a média foi {media}')
+print(f'O maior valor foi {maior} e o menor foi {menor}')
