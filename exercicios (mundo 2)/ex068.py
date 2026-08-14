d04f470398e73afee681aaeb8c654ba14ee3a143
lucasalmeida87222-del <lucasalmeida87222@gmail.com>
2026-08-14 13:34:12 -0300

    Feito alterações

--- exercicios (mundo 2)/ex068.py
+++ exercicios (mundo 2)/ex068.py
@@ -1,2 +1,24 @@
 #Faça um programa que jogue par ou impar com o computador. O jogo só será interrompido quando o jogador perder,
 #mostrando o total de vitórias consecutivas que ele conquistou no final do jogo.
+from random import randint
+cont = 0
+while True:
+    escolha = str(input('Você quer impar ou par? ')).lower().strip()
+    while escolha != 'par' and escolha!= 'impar':
+        escolha = str(input('Você quer impar ou par? ')).lower().strip()
+    jogador = int(input('Digite um valor: '))
+    computador = randint(1,101)
+    print(f'computador: {computador}')
+    soma = jogador + computador
+    print(f'Total: {soma}')
+    if escolha == 'par' and soma % 2 == 0:
+        print('Jogador Venceu!')
+        cont += 1
+    elif escolha == 'impar' and soma % 2 != 0:
+        print('Jogador venceu!')
+        cont += 1
+    else:
+        print('Você perdeu!')
+        break
+    print(f'Contador: {cont}')
+print(f'Numero de vitórias do jogador = {cont}')
--- exercicios (mundo 2)/ex069.py
+++ exercicios (mundo 2)/ex069.py
@@ -1,3 +1,27 @@
 #Crie um programa que leia a idade e o sexo de várias pessoas. A cada pessoa cadastrada, o programa deverá perguntar se o
 #usuário quer ou não continuar. No final mostre: A) Quantas pessoas tem mais de 18 anos. 
 #B) Quantos homens foram cadastrados. C) Quantas mulheres tem menos de 20 anos.
+cont_h = 0
+cont_p = 0
+cont_m = 0
+while True:
+    idade = int(input('Qual a sua idade: '))
+    sexo = str(input('Qual seu gênero [M/F]: ')).upper().strip()
+    while sexo != 'M' and sexo != 'F':
+        print('Opção invalida!')
+        sexo = str(input('Qual seu gênero [M/F]: ')).upper().strip()
+    if sexo == 'M':
+        cont_h += 1
+    if sexo == 'F' and idade < 20:
+        cont_m += 1
+    if idade > 18:
+        cont_p += 1
+    continuar = str(input('Você quer continuar [S/N]? ')).upper().strip()
+    while continuar != 'S' and continuar != 'N':
+        print('Opção invalida!')
+        continuar = str(input('Você quer continuar [S/N]? ')).upper().strip()
+    if continuar == 'N':
+        break
+print(f'{cont_p} pessoas tem mais de 18 anos!')
+print(f'Foram cadastrados {cont_h} homens!')
+print(f'{cont_m} mulheres tem menos de 20 anos!')
