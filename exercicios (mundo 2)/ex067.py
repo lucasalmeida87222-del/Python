f0f372f55a75d33890fe3f443dd85f61e47427c3
lucasalmeida87222-del <lucasalmeida87222@gmail.com>
2026-08-14 09:23:29 -0300

    Fiz alterações

--- exercicios (mundo 2)/ex067.py
+++ exercicios (mundo 2)/ex067.py
@@ -1,8 +1,11 @@
 #Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário.
 #O programa será interrompido quando o número solicitado for negativo.
-cont = 10
 while True:
-    numero = int(input('Digite um numero para ver a sua tabuada: '))
-    if cont <= 10: 
-        cont -= 1
-        print(cont)
+    n = int(input('Quer ver a tabuada de qual valor? '))
+    print('-' * 40)
+    if n < 0:
+        break
+    for c in range (1,11):
+        print(f'{n} X {c} = {n*c}')
+    print('-' * 40)
+print('PROGRAMA TABUADA ENCERRADO. Volte sempre!')
