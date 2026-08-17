1d5ec90e9896b8f4da4f9d70873259a7c60267b6
lucasalmeida87222-del <lucasalmeida87222@gmail.com>
2026-08-17 11:49:13 -0300

    Alterações

--- exercicios (mundo 2)/ex070.py
+++ exercicios (mundo 2)/ex070.py
@@ -1,2 +1,25 @@
 #Crie um programa que leia o nome e o preço de vários produtos. O programa deverá perguntar se o usuário vai continuar. No final, mostre:
 #A) Qual é o total gasto na compra. B) Quantos produtos custam mais de R$1000. C) Qual é o nome do produto mais barato.
+total = 0
+maior = 0
+menor_preco = 0
+produto_barato = ''
+cont = 0 #Contador para identificar o 1º produto
+while True:
+    nome = str(input('Digite o nome de um produto: '))
+    preco = float(input('Digite o valor do produto: '))
+    total += preco
+    cont += 1 #Soma +1 a cada produto lido
+    if cont == 1 or preco < menor_preco:
+        menor_preco = preco
+        produto_barato = nome
+    if preco > 1000:
+        maior += 1
+    continuar = str(input('Você quer continuar [S/N]? ')).upper().strip()
+    while continuar != 'S' and continuar != 'N':
+        print('Opção invalida!' ,end=' ')
+        continuar = str(input('Você quer continuar [S/N]? ')).upper().strip()
+    if continuar == 'N':
+        break
+print(f'O valor total gasto na compra foi de R${total}',end= ' ')
+print(f'{maior} produto(s) são maiores que R$1000,00 e o produto mais barato é {produto_barato}')
--- exercicios (mundo 2)/ex071.py
+++ exercicios (mundo 2)/ex071.py
@@ -1,3 +1,28 @@
 # Crie um programa que simule o funcionamento de um caixa eletrônico. No inicio, pergunte qual será o valor sacado (numero inteiro)
 # e o programa vai informar quantas cédulas de cada valor serão entregues.
 #OBS: Considere que o caixa possui cédulas de R$50, R$20,R$10 e R$1. 
+
+print('-' * 20)
+print('Caixa eletronico')
+print('-' * 20)
+saque = int(input('Qual o valor que deseja sacar? R$'))
+total = saque
+ced = 50
+totced = 0
+while True:
+    if total >= ced:
+        total -= ced
+        totced += 1
+    else:
+        if totced > 0:
+            print(f'Total de {totced} cédulas de R${ced}')
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        totced = 0
+        if total == 0:
+            break
+print('Volte sempre! Tenha um bom dia!')
