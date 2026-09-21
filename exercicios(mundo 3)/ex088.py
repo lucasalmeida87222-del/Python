56465ff5b0d47fbedc1ec2e18202e789060323eb
lucasalmeida87222-del <lucasalmeida87222@gmail.com>
2026-09-21 10:37:36 -0300

    atualizado

--- exercicios(mundo 3)/ex088.py
+++ exercicios(mundo 3)/ex088.py
@@ -1,4 +1,28 @@
 #Faça um programa que ajude um jogador da MEGA SENA a criar palpites. O programa vai perguntar quantos jogos serão gerados e vai sortear 6 números entre 1 e 60 para cada jogo, cadastrando tudo em uma lista composta.
 from random import randint
-
-jogadas = int(input('Quantas jogos você ira realizar: '))
+from time import sleep
+lista = []
+jogos = []
+print('-' * 30)
+print('      JOGA NA MEGA SENA      ')
+print('-' * 30)
+quant = int(input('Quantos jogos você quer que eu sorteie? '))
+tot = 1
+while tot <= quant:
+    cont = 0
+    while True:
+        num = randint(1 , 60)
+        if num not in lista:
+            lista.append(num)
+            cont += 1
+        if cont >= 6:
+            break
+    lista.sort()
+    jogos.append(lista.copy())
+    lista.clear()
+    tot += 1
+print('-=-' * 3, f'SORTEANDO {quant} JOGOS ', '-=-' * 3)
+for indice, l in enumerate(jogos):
+    print(f'Jogo {indice+1}: {l}')
+    sleep(2)
+print('-=-' * 5, '< BOA SORTE! >', '-=-' * 5)
